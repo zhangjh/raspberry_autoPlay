6791df4b5948d11548d6135d684ebcd98d125a5c
zhangjh <njhxzhangjihong@126.com>
2017-07-31 16:08:13 +0800

    Update getWeather.py

--- autoPlay/getWeather.py
+++ autoPlay/getWeather.py
@@ -9,7 +9,7 @@ import json
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-url = "http://api.map.baidu.com/telematics/v3/weather?location=" + argv[1] + "&output=json&ak=E577a29e33bc8df7619f6c364501fd09"
+url = "http://api.map.baidu.com/telematics/v3/weather?location=" + argv[1] + "&output=json&ak=开放平台API key"
 req = urllib2.Request(url)
 #print req
 
